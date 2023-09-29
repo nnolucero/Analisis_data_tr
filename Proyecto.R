7fe2956fbb3143aa535834dc1da8fad7fda36c68
nnolucero <nnolucero@gmail.com>
2023-09-28 22:19:21 -0300

    Se creo la app en Shiny y se elimino los datos vacios(Posiciones abiertas)

--- Proyecto.R
+++ Proyecto.R
@@ -1,26 +1,34 @@
 library(shiny)
+library(dplyr)
 
 ui <- fluidPage(
-  # Crear un elemento de entrada para seleccionar archivos
-  fileInput("files", "Selecciona archivos CSV", multiple = TRUE),
-  # Puedes agregar otros elementos y la interfaz de usuario que necesites aquí
-  # ...
+  titlePanel("Cargar y Filtrar Archivo CSV"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Selecciona un archivo CSV")
+    ),
+    
+    mainPanel(
+      tableOutput("tabla_datos")
+    )
+  )
 )
 
 server <- function(input, output) {
-  # Cuando se seleccionen archivos, puedes trabajar con ellos en el servidor
-  observe({
-    if (!is.null(input$files)) {
-      # input$files contiene la información sobre los archivos seleccionados
-      # Puedes acceder a los nombres y rutas de los archivos seleccionados
-      file_paths <- input$files$datapath
-      
-      # Aquí puedes leer e importar cada archivo CSV en un objeto de datos
-      for (file_path in file_paths) {
-        data <- read.csv(file_path)
-        # Realiza operaciones con data (por ejemplo, almacenarlos en una lista o dataframe)
-      }
-    }
+  datos <- reactive({
+    req(input$file)
+    data <- read.csv(input$file$datapath)
+    
+    # Eliminar filas con valores NA o vacíos en cualquier columna
+    data <- na.omit(data)
+    
+    return(data)
+  })
+  
+  output$tabla_datos <- renderTable({
+    req(datos())
+    datos()
   })
 }
 
